48cae0369b636c7ea6cca4b7703dbbb6d1b457e8
Ilya Yushenko <ilya.y@dev.tagsoft.co>
2024-06-18 21:00:56 +0300

    Fix bug stop app after recive command

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -22,9 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <ctype.h>
-#include "stdio.h"
-#include "string.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,89 +43,101 @@
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
 
-osThreadId LEDUpdateTaskHandle;
-osThreadId UARTTaskHandle;
-osMessageQId UartQueueHandle;
-osSemaphoreId UartSemaphoreHandle;
+osThreadId blinkLEDsTaskHandle;
+osThreadId uartCommandTaskHandle;
 /* USER CODE BEGIN PV */
-#define NUM_LEDS 4                		    // Number of LEDs
-#define FULL_PERIOD 1000          		    // Full period in milliseconds (1 second)
-#define TIMER_PERIOD 250          		    // Timer period (250 ms)
-#define HALF_PERIOD (FULL_PERIOD / 2) 	  // Half cycle (500 ms)
+#define NUM_LEDS 4                		// Number of LEDs
+#define FULL_PERIOD 1000          		// Full period in milliseconds (1 second)
+#define TIMER_PERIOD 250          		// Timer period (250 ms)
+#define HALF_PERIOD (FULL_PERIOD / 2) 	// Half cycle (500 ms)
 
-#define MIN_FREQUENCY 0.1				          // Min frequency (0.1 Hz)
-#define MAX_FREQUENCY 9.9         		    // Max frequency (9.9 Hz)
-#define RX_BUFFER_SIZE 16         		    // UART receive buffer size
+#define MIN_FREQUENCY 0.1				// Min frequency (0.1 Hz)
+#define MAX_FREQUENCY 9.9         		// Max frequency (9.9 Hz)
+#define RX_BUFFER_SIZE 16         		// UART receive buffer size
 
-#define NUM_TASKS 3
+uint8_t led_states[NUM_LEDS];			// Array for storing LED states
+uint16_t led_counter = 0;     			// LED cycling counter
+uint16_t phase_shift = HALF_PERIOD;		// Initial phase shift (180 degrees)
 
-static uint8_t led_states[NUM_LEDS];			          // Array for storing LED states
-static uint16_t led_counter = 0;     			          // LED cycling counter
-static uint16_t phase_shift = HALF_PERIOD;		      // Initial phase shift (180 degrees)
-
-static float frequency = 1.0;      			            // Starting frequency (1 Hz)
-static TickType_t led_update_period = pdMS_TO_TICKS(TIMER_PERIOD); // Time period in milliseconds
+float frequency = 1.0;      			// Starting frequency (1 Hz)
 
 uint8_t rx_buffer[RX_BUFFER_SIZE];
 uint8_t command_received = 0;
 uint8_t rx_index = 0;
-
+volatile uint8_t btn_pressed_flag = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART2_UART_Init(void);
-void StartLedUpdateTask(void const * argument);
-void StartUartTask(void const * argument);
+void StartBlinkLEDsTask(void const * argument);
+void StartUARTCommandTask(void const * argument);
 
 /* USER CODE BEGIN PFP */
-void processCommand(const uint8_t* rx_cmd);
-void updateTimerPeriod(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void commandProcessing(void) {
+	char command[RX_BUFFER_SIZE];
+	strncpy(command, rx_buffer, RX_BUFFER_SIZE);
+	command[RX_BUFFER_SIZE - 1] = 0;
 
-void updateTimerPeriod(void) {
-  uint16_t timer_period = ((uint16_t) (frequency * 1000)); // Time period in milliseconds
-  led_update_period = pdMS_TO_TICKS(timer_period / 4); // Update the LED update period
-  char message[32];
-  sprintf(message, "Led updated period changed to %d ms\r\n", timer_period / 4);
-  HAL_UART_Transmit(&huart2, (uint8_t*) message, strlen(message), HAL_MAX_DELAY);
+	for (uint8_t i = 0; i < strlen(command); i++) {
+		command[i] = tolower(command[i]);
+	}
+
+	if (rx_index != command_received) {
+		double new_frequency;
+		if (sscanf(command, "f=%lf", &new_frequency) == 1) {
+			if (new_frequency >= MIN_FREQUENCY && new_frequency <= MAX_FREQUENCY) {
+				frequency = new_frequency;
+				char message[32];
+				sprintf(message, "Frequency changed to %.1f Hz\r\n", frequency);
+				HAL_UART_Transmit(&huart2, (uint8_t*) message, strlen(message),
+						HAL_MAX_DELAY);
+			} else {
+				char error_message[] =
+						"Error: Frequency must be in the range from 0.1 to 9.9 Hz\r\n";
+				HAL_UART_Transmit(&huart2, (uint8_t*) error_message,
+						strlen(error_message), HAL_MAX_DELAY);
+			}
+		} else {
+			char error_message[] =
+					"Error: Invalid command format. Use the format 'F=x.x'\r\n";
+			HAL_UART_Transmit(&huart2, (uint8_t*) error_message,
+					strlen(error_message), HAL_MAX_DELAY);
+		}
+	}
+
+	rx_index = 0;
+	command_received = 0;
 }
 
-void processCommand(const uint8_t* command) {
-  char cmd[RX_BUFFER_SIZE];
-  strncpy(cmd, command, RX_BUFFER_SIZE);
-  cmd[RX_BUFFER_SIZE - 1] = 0; // Guarantee a null character at the end
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+	if (huart->Instance == USART2) {
+		rx_buffer[rx_index++] = huart->Instance->RDR;
 
-  // Here is the command to restore the registry
-  for (uint8_t i = 0; i < strlen(cmd); i++) {
-    cmd[i] = tolower(cmd[i]);
-  }
+		if (rx_buffer[rx_index - 1] == '\n'
+				|| rx_buffer[rx_index - 1] == '\r') {
+			rx_buffer[rx_index - 1] = 0;
+			command_received = 1;
+			commandProcessing();
+		} else if (rx_index == RX_BUFFER_SIZE - 1) {
+			rx_index = 0;
+		}
 
-  if (rx_index != command_received) {
-    double new_frequency;
-    if (sscanf(cmd, "f=%lf", &new_frequency) == 1) {
-      if (new_frequency >= MIN_FREQUENCY && new_frequency <= MAX_FREQUENCY) {
-        frequency = new_frequency;
-        updateTimerPeriod();
-        char message[32];
-        sprintf(message, "Frequency changed to %.1f Hz\r\n", frequency);
-        HAL_UART_Transmit(&huart2, (uint8_t*)message, strlen(message), HAL_MAX_DELAY);
-      } else {
-        char error_message[] = "Error: Frequency must be in the range from 0.1 to 9.9 Hz\r\n";
-        HAL_UART_Transmit(&huart2, (uint8_t*)error_message, strlen(error_message), HAL_MAX_DELAY);
-      }
-    } else {
-      char error_message[] = "Error: Invalid command format. Use the format 'F=x.x'\r\n";
-      HAL_UART_Transmit(&huart2, (uint8_t*)error_message, strlen(error_message), HAL_MAX_DELAY);
-    }
-  }
+		HAL_UART_Receive_IT(&huart2, (uint8_t*) &rx_buffer[rx_index], 1);
+	}
+}
 
-  rx_index = 0;
-  command_received = 0;
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
+	if (GPIO_Pin == B1_Pin) {
+		HAL_GPIO_TogglePin(LED_MODE_GPIO_Port, LED_MODE_Pin);
+		phase_shift =
+				(phase_shift == HALF_PERIOD) ? HALF_PERIOD / 2 : HALF_PERIOD;
+	}
 }
 /* USER CODE END 0 */
 
@@ -162,45 +172,36 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
+
   /* USER CODE END 2 */
 
   /* USER CODE BEGIN RTOS_MUTEX */
-  /* add mutexes, ... */
+	/* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
 
-  /* Create the semaphores(s) */
-  /* definition and creation of UartSemaphore */
-  osSemaphoreDef(UartSemaphore);
-  UartSemaphoreHandle = osSemaphoreCreate(osSemaphore(UartSemaphore), 1);
-
   /* USER CODE BEGIN RTOS_SEMAPHORES */
-  /* add semaphores, ... */
+	/* add semaphores, ... */
   /* USER CODE END RTOS_SEMAPHORES */
 
   /* USER CODE BEGIN RTOS_TIMERS */
-  /* start timers, add new ones, ... */
+	/* start timers, add new ones, ... */
   /* USER CODE END RTOS_TIMERS */
 
-  /* Create the queue(s) */
-  /* definition and creation of UartQueue */
-  osMessageQDef(UartQueue, 1, uint8_t);
-  UartQueueHandle = osMessageCreate(osMessageQ(UartQueue), NULL);
-
   /* USER CODE BEGIN RTOS_QUEUES */
-  /* add queues, ... */
+	/* add queues, ... */
   /* USER CODE END RTOS_QUEUES */
 
   /* Create the thread(s) */
-  /* definition and creation of LEDUpdateTask */
-  osThreadDef(LEDUpdateTask, StartLedUpdateTask, osPriorityNormal, 0, 128);
-  LEDUpdateTaskHandle = osThreadCreate(osThread(LEDUpdateTask), NULL);
+  /* definition and creation of blinkLEDsTask */
+  osThreadDef(blinkLEDsTask, StartBlinkLEDsTask, osPriorityNormal, 0, 128);
+  blinkLEDsTaskHandle = osThreadCreate(osThread(blinkLEDsTask), NULL);
 
-  /* definition and creation of UARTTask */
-  osThreadDef(UARTTask, StartUartTask, osPriorityNormal, 0, 128);
-  UARTTaskHandle = osThreadCreate(osThread(UARTTask), NULL);
+  /* definition and creation of uartCommandTask */
+  osThreadDef(uartCommandTask, StartUARTCommandTask, osPriorityNormal, 0, 128);
+  uartCommandTaskHandle = osThreadCreate(osThread(uartCommandTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
-  /* add threads, ... */
+	/* add threads, ... */
   /* USER CODE END RTOS_THREADS */
 
   /* Start scheduler */
@@ -210,11 +211,11 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1) {
+	while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+	}
   /* USER CODE END 3 */
 }
 
@@ -342,102 +343,65 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
-{
-  if (GPIO_Pin == B1_Pin) {
-    HAL_GPIO_TogglePin(LED_MODE_GPIO_Port, LED_MODE_Pin);
-    phase_shift = (phase_shift == HALF_PERIOD) ? HALF_PERIOD / 2 : HALF_PERIOD;
-  }
-}
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
-  if (huart->Instance == USART2) { // Check that this is an interrupt from the desired UART
-    rx_buffer[rx_index++] = huart->Instance->RDR; // Write the received byte to the buffer
-
-    // Check if the received byte is a newline character
-    if (rx_buffer[rx_index - 1] == '\n' || rx_buffer[rx_index - 1] == '\r') {
-      rx_buffer[rx_index - 1] = 0; // Replace the newline character with a null character
-      command_received = 1; // Set the flag that the command has been received
-
-      BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-      xQueueSendFromISR(UartQueueHandle, 1, &xHigherPriorityTaskWoken);
-      portYIELD_FROM_ISR(xHigherPriorityTaskWoken);
-    } else if (rx_index == RX_BUFFER_SIZE - 1) {
-      rx_index = 0; // Reset the buffer index if the buffer is full
-    }
-
-    HAL_UART_Receive_IT(&huart2, (uint8_t *)&rx_buffer[rx_index], 1);
-  }
-}
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_StartLedUpdateTask */
+/* USER CODE BEGIN Header_StartBlinkLEDsTask */
 /**
-  * @brief  Function implementing the LEDUpdateTask thread.
-  * @param  argument: Not used
-  * @retval None
-  */
-/* USER CODE END Header_StartLedUpdateTask */
-void StartLedUpdateTask(void const * argument)
+ * @brief  Function implementing the blinkLEDsTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartBlinkLEDsTask */
+void StartBlinkLEDsTask(void const * argument)
 {
   /* USER CODE BEGIN 5 */
-  /* Infinite loop */
-  for (;;)
-  {
-    vTaskDelay(led_update_period);
+	/* Infinite loop */
+	for (;;) {
+		for (int i = 0; i < NUM_LEDS; i++) {
+			uint16_t offset = i * phase_shift;
+			uint16_t phase = (led_counter + offset) % FULL_PERIOD;
 
-    for (int i = 0; i < NUM_LEDS; i++)
-    {
-      uint16_t offset = i * phase_shift; // Phase offset for each LED
-      uint16_t phase = (led_counter + offset) % FULL_PERIOD; // LED Phase Calculation
+			if (phase < HALF_PERIOD) {
+				led_states[i] = 1;
+			} else {
+				led_states[i] = 0;
+			}
+		}
 
-      // Checking the condition for turning the LED on/off
-      if (phase < HALF_PERIOD)
-      {
-        led_states[i] = 1; // Turn on LED
-      }
-      else
-      {
-        led_states[i] = 0; // Turn off LED
-      }
-    }
+		led_counter += TIMER_PERIOD;
+		if (led_counter >= FULL_PERIOD) {
+			led_counter = 0;
+		}
 
-    led_counter += TIMER_PERIOD; // Increment the counter by the timer period for the next iteration
-    if (led_counter >= FULL_PERIOD)
-    {
-      led_counter = 0; // Reset the counter after a full period
-    }
+		HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, led_states[0] ? GPIO_PIN_SET : GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, led_states[1] ? GPIO_PIN_SET : GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, led_states[2] ? GPIO_PIN_SET : GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, led_states[3] ? GPIO_PIN_SET : GPIO_PIN_RESET);
 
-    // LED control
-    HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, led_states[0] ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, led_states[1] ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(LED3_GPIO_Port, LED3_Pin, led_states[2] ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(LED4_GPIO_Port, LED4_Pin, led_states[3] ? GPIO_PIN_SET : GPIO_PIN_RESET);
-  }
+		float period = (1 / frequency);
+		uint16_t timer_period = (uint16_t) (period * 1000);
+		osDelay((uint32_t) (timer_period / 4));
+	}
   /* USER CODE END 5 */
 }
 
-/* USER CODE BEGIN Header_StartUartTask */
+/* USER CODE BEGIN Header_StartUARTCommandTask */
 /**
-* @brief Function implementing the UARTTask thread.
-* @param argument: Not used
-* @retval None
-*/
-/* USER CODE END Header_StartUartTask */
-void StartUartTask(void const* argument)
+ * @brief Function implementing the uartCommandTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartUARTCommandTask */
+void StartUARTCommandTask(void const * argument)
 {
-  /* USER CODE BEGIN StartUartTask */
-  HAL_UART_Receive_IT(&huart2, (uint8_t*)rx_buffer, 1);
-  uint8_t received_event;
-  /* Infinite loop */
-  for (;;)
-  {
-    if (xQueueReceive(UartQueueHandle, &received_event, portMAX_DELAY) == pdPASS)
-    {
-      processCommand(rx_buffer);
-    }
-  }
-  /* USER CODE END StartUartTask */
+  /* USER CODE BEGIN StartUARTCommandTask */
+	HAL_UART_Receive_IT(&huart2, (uint8_t*) rx_buffer, 1);
+	/* Infinite loop */
+	for (;;) {
+		osDelay(1);
+	}
+  /* USER CODE END StartUARTCommandTask */
 }
 
 /**
@@ -451,6 +415,7 @@ void StartUartTask(void const* argument)
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
   /* USER CODE BEGIN Callback 0 */
+
   /* USER CODE END Callback 0 */
   if (htim->Instance == TIM17) {
     HAL_IncTick();
@@ -467,11 +432,10 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
   /* USER CODE END Error_Handler_Debug */
 }
 
